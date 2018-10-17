2511a46653ad3822753bc98fd7ba7055c596c46c
kevin115533 <kevin115533@yahoo.com>
2018-10-17 14:52:44 -0500

    throw and catch works

--- JavaLessonSix.java
+++ JavaLessonSix.java
@@ -1,25 +1,20 @@
+import java.io.*;
+
 public class JavaLessonSix{
 	public static void main(String[] args){
-	
-	divideByZero(2);
-	}
-	
-	public static void divideByZero(int x){
-	
-	try{
-	System.out.println(x/0);
-	}
-	
-	catch(ArithmeticException e){
-		System.out.println("You cant do that");
+		try{
+		getAFile("./somestuff.txt");
+		}
 		
-		System.out.println(e.getMessage());
-		
-		System.out.println(e.toString());
-						   
-		e.printStackTrace();
+		catch(IOException e){
+			System.out.println("an IO error occured");	
+		}
+			
 	}
+				   
 	
+	public static void getAFile(String fileName) throws IOException, FileNotFoundException{
+		FileInputStream file = new FileInputStream(fileName);
 	
 	}
 }
